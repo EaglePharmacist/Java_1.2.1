bd98ce3b78870aa881ae8f216eec8a5d864895b8
Julia <J@mail.ru>
2025-07-02 19:41:57 +0700

    change main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,18 @@
 public class Main {
-    
+    public static void main(String[] args) {
+
+        int clientMoney = 100;
+        int plus = 1100;
+        int sum = clientMoney + plus;
+        int bonus = plus / 100;
+        int moneyPlusBonus = clientMoney + plus + bonus;
+
+        if (plus > 1000){
+            System.out.println("Итоговая сумма бонуса составила: " + bonus);
+            System.out.println("Итоговый баланс счёта составил: " + moneyPlusBonus);
+        } else {
+            System.out.println("Итоговая сумма бонуса составила: 0 руб.");
+            System.out.println("Итоговый баланс счёта составил: " + sum);
+        }
+    }
 }
